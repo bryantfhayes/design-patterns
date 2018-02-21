8870e3289301c8612cd89384270f915f51b5801c
Bryant Hayes <bryantfhayes@gmail.com>
2018-02-20 22:22:57 -0800

    remove fill script

--- fill.sh
+++
@@ -1,8 +0,0 @@
-for D in *; do
-    if [ -d "${D}" ]; then
-        echo "${D}"   # your processing here
-        cp -a .template/* ${D}/
-        sed -i -e 's/PROJECT-NAME/'${D}'/g' ${D}/Makefile
-        rm ${D}/Makefile-e
-    fi
-done
